38f806089af37c01c96d58d96a401243dd8ee29f
ManishaPriyadharshiniM <manisha96priya@gmail.com>
2016-07-08 12:01:50 +0530

    finish

---
+++ numbersa.c
@@ -0,0 +1,19 @@
+# numbers
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("enter the numbers");
+scanf("%d %d %d",a,b,c);
+if(a>b&&a>c)
+{
+printf("a is largest",a);
+}
+else
+printf("c is largest",c);
+}
+else
+printf("b is greater",b);
+}
+return 0;
+}
